fix exposure scaling offset

lib/dmpigo.py
@@ -346,10 +346,9 @@ class DirectMPIGO(torch.nn.Module):
             # Force scaling offset to always be zero when exposure_idx is 0.
             # This constraint fixes a reference point for the scene's brightness.
             mask = exp_metadata['exposure_idx'] > 0
-            scaling = 1 + mask[..., None] * self.learned_exposure_scaling(exp_metadata['exposure_idx'])
-            rgb_marched *= scaling * exp_metadata['ShutterSpeed'][..., None]
+            scaling = mask[..., None] * self.learned_exposure_scaling(exp_metadata['exposure_idx'])
+            rgb_marched *= 1 + (scaling * exp_metadata['ShutterSpeed'][..., None])
             rgb_marched = torch.minimum(rgb_marched, torch.ones_like(rgb_marched))
-
 
         s = (step_id+0.5) / N_samples
         ret_dict.update({